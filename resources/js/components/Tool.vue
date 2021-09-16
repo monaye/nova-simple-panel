12a4e051689666887df4d65d6ed332dcd311f432
Monaye Win <>
2021-09-16 07:07:03 -0700

    creating default resource tool

---
+++ resources/js/components/Tool.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Nova Simple Panel</div>
+</template>
+
+<script>
+export default {
+  props: ['resourceName', 'resourceId', 'panel'],
+
+  mounted() {
+    //
+  },
+}
+</script>
---
+++ resources/js/tool.js
@@ -0,0 +1,3 @@
+Nova.booting((Vue, router, store) => {
+  Vue.component('nova-simple-panel', require('./components/Tool'))
+})
